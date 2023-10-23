0fa1ccc7b6a1c017632c15586281082ee3c7a8d8
deepakyadav121999 <dy44444221@gmail.com>
2023-10-23 16:49:54 +0530

    dd

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,78 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import '../Styles/HomePage.css';
-import { BiSolidLike } from 'react-icons/bi';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../Styles/HomePage.css";
+import { BiSolidLike } from "react-icons/bi";
 
-import { FiTwitter } from 'react-icons/fi';
-import {BsInstagram } from 'react-icons/bs';
-import { Skeleton } from '@mui/material';
-
-
+import { FiTwitter } from "react-icons/fi";
+import { BsInstagram } from "react-icons/bs";
+import { Skeleton } from "@mui/material";
 
 const HomePage = () => {
-  const apiUrl = 'https://api.unsplash.com/search/photos';
-  const apiKey = 'slUx6qxOMpexd-Jt1Vhl0DHCTkL5glzI9DpDQd0lGBM';
-  const pageSize = 40;
+  const apiUrl = "https://api.unsplash.com/search/photos";
+  const apiKey = "slUx6qxOMpexd-Jt1Vhl0DHCTkL5glzI9DpDQd0lGBM";
+  const pageSize = 20;
 
   const [searchData, setSearchData] = useState([]);
-  const [inputVal, setInputVal] = useState('');
+  const [inputVal, setInputVal] = useState("");
   const [discription, setDiscription] = useState({
-    imageUrl: '',
-    likes: '',
-    dpUrl: '',
-    username: '',
-    userId: '',
-    instagram: '',
-    twitter: '',
-    downloadCont: '',
-    downloadlink: '',
+    imageUrl: "",
+    likes: "",
+    dpUrl: "",
+    username: "",
+    userId: "",
+    instagram: "",
+    twitter: "",
+    downloadCont: "",
+    downloadlink: "",
   });
-  const [hidden, setHidden] = useState('non-hidden');
-  const [pophidden, popsetHidden] = useState('hidden');
- const[page,setpages] = useState(1);
- const[loading,setloading] = useState(false)
+  const [hidden, setHidden] = useState("non-hidden");
+  const [pophidden, popsetHidden] = useState("hidden");
+  const [page, setpages] = useState(1);
+  const [loading, setloading] = useState(false);
 
   const api = async () => {
-   
     let res = await axios.get(
-      'https://api.unsplash.com/photos/?client_id=slUx6qxOMpexd-Jt1Vhl0DHCTkL5glzI9DpDQd0lGBM'
+      "https://api.unsplash.com/photos/?client_id=slUx6qxOMpexd-Jt1Vhl0DHCTkL5glzI9DpDQd0lGBM"
     );
 
     console.log(res.data);
     setSearchData(res.data);
-
-  }
+  };
 
   const apiCall = async () => {
- setloading(true)
+    setloading(true);
     const result = await axios.get(
       `${apiUrl}?query=${inputVal}&page=${page}&per_page=${pageSize}&client_id=${apiKey}`
     );
     let c = result.data.results;
     setSearchData(c);
-    setloading(false)
- 
+    setloading(false);
   };
 
- useEffect(()=>{
-  setloading(true)
-apiCall()
-setloading(false)
- },[page])
 
   useEffect(() => {
     api();
+    
   }, []);
 
+  useEffect(() => {
+  
+    apiCall();
 
-useEffect(()=>{
-  setloading(true)
-apiCall()
-setloading(false)
-},[inputVal])
+  }, [page]);
   return (
     <>
- 
       <div className={`container ${hidden}`}>
         <h1>Image Search</h1>
         <div className="search-section">
@@ -92,25 +81,23 @@ setloading(false)
         <div className="category-button">
           <p
             onClick={() => {
-              setInputVal('Coding');
-              apiCall()
+              setInputVal("Coding");
+            
             }}
           >
             Coding
           </p>
           <p
             onClick={() => {
-              setInputVal('Pubg');
-              apiCall()
-            
+              setInputVal("Pubg");
+             
             }}
           >
             Pubg
           </p>
           <p
             onClick={() => {
-              setInputVal('Nature');
-              apiCall()
+              setInputVal("Nature");
             
             }}
           >
@@ -118,26 +105,21 @@ setloading(false)
           </p>
           <p
             onClick={() => {
-              setInputVal('Birds');
-              apiCall()
-            
+              setInputVal("Birds");
+             
             }}
           >
             Birds
           </p>
         </div>
 
-
-
-   {loading?(<Skeleton variant="rectangular" width={210} height={250} />
- 
-   ): <div className="screen">
+        <div className="screen">
           {searchData &&
-            searchData.map((image,index) => {
+            searchData.map((image) => {
               return (
                 <div
                   className="display-info"
-                  key={index}
+                 
                   onClick={() => {
                     let im = image.urls.full;
                     setDiscription({
@@ -150,70 +132,100 @@ setloading(false)
                       twitter: image.user.social.twitter_username,
                       downloadlink: image.links.download,
                     });
-                    setHidden('hidden');
-                    popsetHidden("non-hidden")
+                    setHidden("hidden");
+                    popsetHidden("non-hidden");
                   }}
                 >
                   <div className="user-likes">
                     <div className="cnt1">
-  <img src={image.urls.small} alt="" className="main-img" />
+                      {loading ? (
+                        <Skeleton
+                          variant="rectangular"
+                          width={210}
+                          height={250}
+                        />
+                      ) : (
+                        <img
+                          src={image.urls.small}
+                          alt=""
+                          className="main-img"
+                        />
+                      )}
                     </div>
                     <div className="cnt2">
-                      <div className="username">
-                         <img
-                          src={image.user.profile_image.small}
-                          alt=""
-                          className="profile-image"
-                        />
-                      <div className="user-id">
-                          <p>{`${image.user.first_name} ${image.user.last_name}`}</p>
-                         {image.user.username&& <p className="user-id1">@{image.user.username}</p>}
+                     
+                        <div className="username">
+                          <img
+                            src={image.user.profile_image.small}
+                            alt=""
+                            className="profile-image"
+                          />
+                          <div className="user-id">
+                            <p>{`${image.user.first_name} ${image.user.last_name}`}</p>
+                            {image.user.username && (
+                              <p className="user-id1">@{image.user.username}</p>
+                            )}
+                          </div>
                         </div>
-                      </div>
-                      <div className="likes">
-                        <p>
-                          <BiSolidLike />
-                          {image.likes}
-                        </p>
-                      </div>
+                    
+
+                   
+                        <div className="likes">
+                          <p>
+                            <BiSolidLike />
+                            {image.likes}
+                          </p>
+                        </div>
+                   
                     </div>
                   </div>
                 </div>
               );
             })}
-        </div>}
+        </div>
 
-
-
-
-
-
-      <div className="next-prev-btn">
-        <p className='pags-btn' onClick={()=>{
-          if(page>1)
-          setpages(page-1)
-        }}>Prev Page</p>
-        <p className='pags-btn' onClick={()=>{
-          if(page>=1 && page<6){
-            setpages(page+1)
-          }
-        }}>Next Page</p>
-      </div>
-
+        <div className="next-prev-btn">
+          <p
+            className="pags-btn"
+            onClick={() => {
+              if (page > 1) setpages(page - 1);
+            }}
+          >
+            Prev Page
+          </p>
+          <p
+            className="pags-btn"
+            onClick={() => {
+              if (page >= 1 && page < 6) {
+                setpages(page + 1);
+              }
+            }}
+          >
+            Next Page
+          </p>
+        </div>
       </div>
 
       <div className={`hidden-popup ${pophidden} `}>
         <p
           onClick={() => {
-            setHidden('non-hidden');
-            popsetHidden("hidden")
+            setHidden("non-hidden");
+            popsetHidden("hidden");
           }}
-          className='search-btn close-button'
+          className="search-btn close-button"
         >
           Close X
         </p>
         <div className="img-corner">
-         {discription.imageUrl? <img src={discription.imageUrl} alt="" className="hidden-main-img" />:<Skeleton variant="rectangular" width={210} height={60} />}
+          {discription.imageUrl ? (
+            <img
+              src={discription.imageUrl}
+              alt=""
+              className="hidden-main-img"
+            />
+          ) : (
+            <Skeleton variant="rectangular" width={210} height={60} />
+          )}
         </div>
 
         <div className="user-details-handle">
@@ -226,7 +238,7 @@ setloading(false)
               <p className="user-id1">@{discription.userId}</p>
             </div>
             <div className="user-id2">
-              <p className=''>
+              <p className="">
                 <BsInstagram />/{discription.instagram}
               </p>
             </div>
@@ -255,7 +267,7 @@ setloading(false)
             </a>
           </div>
         </div>
-      </div> 
+      </div>
     </>
   );
 };
